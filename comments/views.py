f99e96a3d87f76fa964777c1f8b3ce0630d65122
Artem Hryshko <artemggg2222@gmail.com>
2024-06-19 18:44:21 +0300

    add CommentListView and url

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Comment
+
+
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'comment_list.html'
+    context_object_name = 'comments'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(parent=None)
+        sort_by = self.request.GET.get('sort_by')
+        if sort_by in ['user_name', 'email', 'created_at']:
+            order = self.request.GET.get('order', 'asc')
+            if order == 'desc':
+                sort_by = '-' + sort_by
+            queryset = queryset.order_by(sort_by)
+        return queryset
