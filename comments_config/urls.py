2b26057af30e9e3a9653ee357dce14550afd9113
Artem Hryshko <artemggg2222@gmail.com>
2024-06-20 20:36:42 +0300

    add url for user

--- comments_config/urls.py
+++ comments_config/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('comments.urls')),
     path('captcha/', include('captcha.urls')),
+    path("user/", include('user.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
